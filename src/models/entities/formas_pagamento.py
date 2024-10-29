875aa5a7c19cf0285ade8e9d681baa6dbfdac92b
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-10-28 21:26:33 -0300

    Update formas_pagamento.py

--- src/models/entities/formas_pagamento.py
+++ src/models/entities/formas_pagamento.py
@@ -1,7 +1,7 @@
 from models.configs.base import Base
 from sqlalchemy import Column, Integer, String
 
-class CategoriasReceitas(Base):
-    __tablename__ = "categorias_receitas"
+class FormasPagamento(Base):
+    __tablename__ = "formas_pagamento"
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100), nullable=False)
