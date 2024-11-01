0822ad32ad53557a58baeae1385507e9ae451234
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-11-01 09:24:23 -0300

    conecting api

--- src/run.py
+++ src/run.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from resources.resources import resources
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app, origins="http://localhost:5173")
 
 resources(app)
 
