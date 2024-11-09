81ee97d5642488676e64c0402c6e551e7a52b6be
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2024-11-09 19:42:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, origins=["http://localhost:5173", "http://localhost:3000", "https://despesas-pessoais-web.onrender.com/"])
+CORS(app, origins=["http://localhost:5173", "http://localhost:3000", "https://despesas-pessoais-web.onrender.com"])
 
 resources(app)
 
