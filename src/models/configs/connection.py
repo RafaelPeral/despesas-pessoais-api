0768233358e50f79d3df36abd3bb836221f378ce
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2025-11-12 15:54:49 -0300

    Update database host in DBConnectionHandler

--- src/models/configs/connection.py
+++ src/models/configs/connection.py
@@ -5,7 +5,7 @@ class DBConnectionHendler:
     def __init__(self):
         self.__username = "despesaspessoais_user"
         self.__password = "dZomebCzbEqbtOfT99QOI2kQY4pSFDLO"
-        self.__host = "dpg-csfp8v2j1k6c73b1uek0-a.oregon-postgres.render.com"
+        self.__host = "dpg-d4ad7aje5dus73a22o80-a.oregon-postgres.render.com"
         self.__port = "5432"
         self.__database = "despesaspessoais"
         self.__connection_string = "postgresql://{}:{}@{}/{}".format(self.__username, self.__password, self.__host, self.__database)
