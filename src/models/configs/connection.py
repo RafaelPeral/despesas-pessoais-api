402668247a61112b6f1323a10046319b7019ec7b
Rafael Peral Venet Ferreira <rafaelvenet@gmail.com>
2025-11-12 15:38:01 -0300

    Update database connection credentials

--- src/models/configs/connection.py
+++ src/models/configs/connection.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHendler:
     def __init__(self):
-        self.__username = "despesas_pessoais_db_user"
-        self.__password = "J7CaAZQ0uv8qGMfya1K5oBvvjF78ZzUp"
+        self.__username = "despesaspessoais_user"
+        self.__password = "dZomebCzbEqbtOfT99QOI2kQY4pSFDLO"
         self.__host = "dpg-csfp8v2j1k6c73b1uek0-a.oregon-postgres.render.com"
         self.__port = "5432"
-        self.__database = "despesas_pessoais_db"
+        self.__database = "despesaspessoais"
         self.__connection_string = "postgresql://{}:{}@{}/{}".format(self.__username, self.__password, self.__host, self.__database)
         self.__engine = self.__create_databese_engine()
         self.session = None
